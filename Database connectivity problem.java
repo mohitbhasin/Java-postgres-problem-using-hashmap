d70f2e101428a78d726ca771cd400e8adc5b8241
mohitbhasin <mohit.bhasin16@gmail.com>
2015-05-12 00:14:05 -0400

    Java-postgres Hash map problems
    connecting to posters using java code storing the result in result set
    and manipulating data. (data structure used HashMap)

---
+++ Database connectivity problem.java
@@ -0,0 +1,191 @@
+/*
+Prepared By MOHIT BHASIN.
+CWID 10400111
+
+*About the Program
+-This program will print average sales quantity of each combination of customer and product
+ for NY, NJ, CT separately.
+-Programming language used is JAVA. 
+-The only SQL command used is select * from sales.
+-The output is produced in single scan of ResultSet.
+
+*How to run the program
+-Make sure that JDK is installed on the machine and "sales" table is created in Postgres database.
+-Run the following command in terminal to set CLASSPATH of Postgres driver for accessing Postgres Database
+	export CLASSPATH=/home/user/java/postgresql-9.2-1003.jdbc4.jar:.
+-Type "javac AvgSales.java" in terminal to compile he program.
+-Type "java AvegSales" to run it.
+
+*DataStructure Used
+-The DataStructure used is HashMap. HashMap is chosen because it is based on key-value relations
+	which is easy to create and manage. HashMaps are also very fast.
+
+*Algorithm of the Program
+-A HashMap is created with key column containing "Customers" and value column containing a new HashMap.
+-In the new HashMap, key column contains "Product" and value column contains another HashMap.
+-This third HashMap has "states" in the key and class "Average" in value.
+-First, the result of the Select query is stored in the resultSet.
+-While traversing ResultSet if State is "NY","NJ"or"CT",add customer if it is not in the HashMap.
+-Now call "addProd" function with customer as the parameter, and repeat above step for product.
+-Call "addState" function with customer and product as parameters.
+*/
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.*;
+
+
+public class b 
+{
+//HashMap "map" will store combination of "customer","product" with different "states" and class "Average" 
+	public	static HashMap<String, HashMap<String, HashMap<String,Average>>> map = new 
+			HashMap<String, HashMap<String, HashMap<String,Average>>>();
+	static ResultSet rs;
+	public static void main(String args[])
+	{
+		//String usr ="mbhasin";
+		//String pwd ="Stevens128278";
+		//String url ="jdbc:postgresql://155.246.89.29:5432/mbhasin";
+			String usr ="postgres";
+			String pwd ="12345";
+			String url ="jdbc:postgresql://localhost:5432/postgres";
+			//Loading Postgres driver to establish connection with postgres database
+			try 
+			{
+				Class.forName("org.postgresql.Driver");
+				System.out.println("Success loading Driver!");
+			} 
+			//If any exception occurs in loading driver program will print the below String.
+			catch(Exception e) 
+			{
+				System.out.println("Fail loading Driver!");
+				e.printStackTrace();
+			}
+			//Establishing connection to the database using URL,userID and Password
+			try
+			{
+			Connection conn = DriverManager.getConnection(url, usr, pwd);
+			System.out.println("Success connecting server!");
+			//Executing the Select Query and putting its result in ResultSet.
+			Statement stmt = conn.createStatement();
+			rs = stmt.executeQuery("SELECT * FROM sales");
+			while(rs.next())
+			{
+				//This Condition will check for NY,NJ and CT states
+				if (rs.getString("state").equals("NJ") || 
+					rs.getString("state").equals("NY") || 
+					rs.getString("state").equals("CT"))
+				{
+				//If States are either NY,NJ or CT, this condition will add customer to the key column
+				//	of HashMap if they don't exist.
+				//	After adding customer new HashMap will be created for adding product.
+				if(!(map.containsKey(rs.getString("cust"))))
+						{
+						map.put(rs.getString("cust"), new HashMap<String,HashMap<String,Average>>());
+						}
+				//Function addProd is called to add products with customer as the argument
+				addProd(rs.getString("cust"));
+			}
+			}
+			//Finally print_data() will print the table with customer,product and state wise average of product   
+			conn.close();
+			print_data();
+			}
+			//If any exception occurs in above try clause, it will print the following string. 
+			catch(SQLException e) 
+			{
+				System.out.println("Connection URL or username or password errors!");
+				e.printStackTrace();
+			}
+			}
+	
+//addProd() will accept customer as argument. If combination of customer and product does not exist in HashMap
+// it will put product in the map and create new HashMap for states and class average. 	
+public static void addProd(String customer) throws SQLException
+{
+	if(!(map.get(customer).containsKey(rs.getString("prod"))))
+	{
+	map.get(customer).put(rs.getString("prod"), new HashMap<String,Average>());
+	}
+//addState() is called with customer and product as arguments.	
+	addState(customer,rs.getString("prod"));
+}
+//first, addState() will check for state, if state already exist then object of class Average will
+// will update total count and total quantity by calling getCount() and getQuantity()
+public static void addState(String customer,String product) throws SQLException
+{
+	if(map.get(customer).get(product).containsKey(rs.getString("state")))
+	{
+	Average comb=	map.get(customer).get(product).get(rs.getString("state"));
+	comb.setCount(comb.getCount() + 1);
+	comb.setQuantity(comb.getQuantity() + rs.getInt("quant"));
+	map.get(customer).get(product).put(rs.getString("state"), comb);
+	}
+	else
+	//If the state does not exist for the combination of customer and product then new state will be
+	// inserted with total count set to "1" and its quantity.	
+	{
+		map.get(customer).get(product).put(rs.getString("state"), new Average(1,rs.getInt("quant")));	
+	}
+}
+//print_data() will print the entire HashMap 
+public static void print_data()
+{
+	System.out.println("CUSTOMER"+" PRODUCT"+"      NY_AVG"+"     NJ_AVG"+"     CT_AVG");
+	System.out.println("========"+" ======="+"     ======="+"    ======="+"    =======");
+	//For ever customer 
+	for(String customer: map.keySet())
+	{
+		for(String product: map.get(customer).keySet())
+		{
+			double ny=0.0;
+			double nj= 0.0;
+			double ct= 0.0;
+			for(String state:map.get(customer).get(product).keySet())
+			{
+				Average comb=map.get(customer).get(product).get(state);
+				if(state.equals("NY"))
+					ny=comb.getQuantity()/comb.getCount();
+				if(state.equals("NJ"))
+					nj=comb.getQuantity()/comb.getCount();
+				if(state.equals("CT"))
+					ct=comb.getQuantity()/comb.getCount();
+			}
+			System.out.printf("%-8s %-8s %10.2f %10.2f %10.2f %n",customer,product,ny,nj,ct);
+		}
+	}
+}
+
+}
+//class Average will is use to initialize count and tot_quant variables.
+class Average {
+	int count;
+	double tot_quant;
+//This is default constructor. It will be called automatically
+//	whenever object is created and will set values of count and tot_quant to "0".
+	public Average() {
+		this.count = 0;
+		this.tot_quant = 0.0;
+	} 
+//This function will set the new values.	
+	public Average(int count, double quant) {
+		this.count = count;
+		this.tot_quant = quant;
+	} 
+//the following functions are used to set and retrieve the values of count and quantity.
+	public void setCount(int value) {
+		this.count = value;
+	}
+	public void setQuantity(double value) {
+		this.tot_quant = value;
+	}
+	public int getCount() {
+		return this.count;
+	} 
+	public double getQuantity() {
+		return this.tot_quant;
+	}
+}
---
+++ report_3.java
@@ -0,0 +1,319 @@
+/*
+Report 3
+Prepared By MOHIT BHASIN.
+CWID 10400111
+
+
+ *About the Program
+-This program will print number of quantity which are between that quater's average sales 
+and maximum sales quantities.
+-Programming language used is JAVA. 
+-The only SQL command used is select * from sales.
+-The output is produced in single scan of ResultSet.
+
+ *How to run the program
+-Make sure that JDK is installed on the machine and "sales" table is created in Postgres database.
+-Run the following command in terminal to set CLASSPATH of Postgres driver for accessing Postgres Database
+	export CLASSPATH=/home/user/java/postgresql-9.2-1003.jdbc4.jar:.
+-Type "javac report_2.java" in terminal to compile he program.
+-Type "java report_2" to run it.
+
+ *DataStructure Used
+-The DataStructure used is HashMap. HashMap is chosen because it is based on key-value relations
+	which is easy to create and manage. HashMaps are also very fast.
+-Arraylist is also used for storing Maximum quantities.
+
+ *Algorithm of the Program
+-A HashMap is created with key column containing "Customers" and value column containing a new HashMap.
+-Another HashMap is created with customer,product and ArrayList for quantities.
+-In both HashMap, key column contains "Product" and value column contains null initially.
+-New HashMap Quarter is created with keys as Q1,Q2,Q3,Q4 and new average object with values 0,0.
+-Now values of sales quantities are added in HashMAp quarter on the bases of months.
+-Then the above hashMap is added to the original HashMap containing combination of customer & product.  
+-Finally, calculate the averages and print the data.
+ */
+
+import java.sql.*;
+import java.util.*;
+
+public class report_3
+
+{
+	// global ResultSet declaration
+	private static ResultSet rs = null;
+	// Declaration of data structure used for average and maximumimum quantities
+	// in this program i.e HashMap
+	private static HashMap<String, HashMap<String, HashMap<String, Average>>> map = new HashMap<String, HashMap<String, HashMap<String, Average>>>();
+	// Declaration of data structure used for storing all quantities in this
+	// program i.e HashMap
+	private static HashMap<String, HashMap<String, HashMap<String, ArrayList<Double>>>> map2 = new HashMap<String, HashMap<String, HashMap<String, ArrayList<Double>>>>();
+
+	// executable method
+	public static void main(String[] args) {
+		String usr = "postgres";
+		String pwd = "12345";
+		String url = "jdbc:postgresql://localhost:5432/postgres";
+		try {
+			Class.forName("org.postgresql.Driver");
+			System.out.println("Success loading Driver!");
+		}
+
+		catch (Exception e) {
+			System.out.println("Fail loading Driver!");
+			e.printStackTrace();
+		}
+
+		try {
+			Connection conn = DriverManager.getConnection(url, usr, pwd);
+			System.out.println("Success connecting server!");
+
+			Statement stmt = conn.createStatement();
+			// executing query
+			rs = stmt.executeQuery("SELECT * FROM Sales");
+
+			while (rs.next()) {
+				// this condition checks whether customer is already added, if
+				// not then new customer will be added
+				if (!(map.containsKey(rs.getString("cust")))) {
+					map.put(rs.getString("cust"),
+							new HashMap<String, HashMap<String, Average>>());
+					map2.put(
+							rs.getString("cust"),
+							new HashMap<String, HashMap<String, ArrayList<Double>>>());
+				}
+				// calling addProduct method passing parameter customer name
+				addProd(rs.getString("cust"));
+			}
+			// print method is called for outputting data
+			print_data();
+			// if exception is thrown from above code then catch will catch the
+			// exception and print stack trace of exception
+		} catch (SQLException e) {
+			System.out
+					.println("Connection URL or username or password errors!");
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 
+	 * @param cust
+	 * @throws SQLException
+	 *             method for adding Product for particular Customer it will
+	 *             check if Customer already contains Product, if not then add
+	 *             Product as key to HashMap and corresponding value i.e other
+	 *             HashMap and call addQuarter method if Customer already
+	 *             contains Product then it will directly call addQuarter method
+	 */
+
+	private static void addProd(String customer) throws SQLException {
+		// this condition checks whether product is already added, if not then
+		// new product will be added
+		if (!(map.get(customer).containsKey(rs.getString("prod")))) {
+			map.get(customer).put(rs.getString("prod"), null);
+			map2.get(customer).put(rs.getString("prod"), null);
+		}
+		// calling function addQuarter with parameter customer name and product
+		// name
+		Quarter(customer, rs.getString("prod"));
+	}
+
+	/**
+	 * 
+	 * @param cust
+	 * @param prod
+	 * @throws SQLException
+	 *             this method will add quantity to corresponding customer,
+	 *             product and quarter i.e if month <=3 then add to quarter_1
+	 *             and so on
+	 */
+
+	private static void Quarter(String customer, String product)
+			throws SQLException {
+		if (map.get(customer).get(product) == null) {
+			HashMap<String, Average> quarters = new HashMap<String, Average>();
+			quarters.put("Q1", new Average(0, 0.0));
+			quarters.put("Q2", new Average(0, 0.0));
+			quarters.put("Q3", new Average(0, 0.0));
+			quarters.put("Q4", new Average(0, 0.0));
+			map.get(customer).put(product, quarters);
+			HashMap<String, ArrayList<Double>> quarters2 = new HashMap<String, ArrayList<Double>>();
+			quarters2.put("Q1", new ArrayList<Double>());
+			quarters2.put("Q2", new ArrayList<Double>());
+			quarters2.put("Q3", new ArrayList<Double>());
+			quarters2.put("Q4", new ArrayList<Double>());
+			map2.get(customer).put(product, quarters2);
+		}
+		HashMap<String, Average> quarters = map.get(customer).get(product);
+		if (rs.getInt("month") < 4) {
+			Average comb = quarters.get("Q1");
+			int count = comb.getCount();
+			double quantity = comb.getQuantity();
+			quantity = quantity + rs.getInt("quant");
+			count = count + 1;
+			comb.setCount(count);
+			comb.setQuantity(quantity);
+			quarters.put("Q1", comb);
+			map2.get(customer).get(product).get("Q1")
+					.add(rs.getInt("quant") + 0.0);
+		} else if (rs.getInt("month") >= 7 && rs.getInt("month") < 10) {
+			Average comb = quarters.get("Q3");
+			int count = comb.getCount();
+			double quantity = comb.getQuantity();
+			quantity = quantity + rs.getInt("quant");
+			count = count + 1;
+			comb.setCount(count);
+			comb.setQuantity(quantity);
+			quarters.put("Q3", comb);
+			map2.get(customer).get(product).get("Q3")
+					.add(rs.getInt("quant") + 0.0);
+		} else if (rs.getInt("month") >= 4 && rs.getInt("month") < 7) {
+			Average comb = quarters.get("Q2");
+			int count = comb.getCount();
+			double quantity = comb.getQuantity();
+			quantity = quantity + rs.getInt("quant");
+			count = count + 1;
+			comb.setCount(count);
+			comb.setQuantity(quantity);
+			quarters.put("Q2", comb);
+			map2.get(customer).get(product).get("Q2")
+					.add(rs.getInt("quant") + 0.0);
+		} else {
+			Average comb = quarters.get("Q4");
+			int count = comb.getCount();
+			double quantity = comb.getQuantity();
+			quantity = quantity + rs.getInt("quant");
+			count = count + 1;
+			comb.setCount(count);
+			comb.setQuantity(quantity);
+			quarters.put("Q4", comb);
+			map2.get(customer).get(product).get("Q4")
+					.add(rs.getInt("quant") + 0.0);
+		}
+		map.get(customer).put(product, quarters);
+	}
+
+	/**
+	 * method for printing HashMap (data) in required output using format method
+	 * of String class for formating data in required output
+	 */
+
+	private static void print_data() {
+		System.out.println("CUSTOMER" + " PRODUCT" + " QUARTER" + " BEFORE_TOT"
+				+ " AFTER_TOT");
+		System.out.println("========" + " =======" + " =======" + " =========="
+				+ " =========");
+
+		for (String customer : map.keySet()) {
+			for (String product : map.get(customer).keySet()) {
+				for (String quarter : map.get(customer).get(product).keySet()) {
+					if (quarter.equals("Q1")) {
+						int after = 0;
+						double avg = map.get(customer).get(product).get("Q1")
+								.getQuantity()
+								/ map.get(customer).get(product).get("Q1")
+										.getCount();
+						double maximum = 0.0;
+						for (double quant : map2.get(customer).get(product)
+								.get("Q1")) {
+							if (quant > maximum) {
+								maximum = quant;
+							}
+						}
+						for (double quant : map2.get(customer).get(product)
+								.get("Q2")) {
+							if (quant >= avg && quant <= maximum) {
+								after++;
+							}
+						}
+						System.out
+								.printf("%-8s %-7s %-7s %-10s %9s %n",
+										customer, product, quarter,
+										"    <NULL>", after);
+
+					} else if (quarter.equals("Q2")) {
+						int before = 0;
+						int after = 0;
+						double avg = map.get(customer).get(product).get("Q2")
+								.getQuantity()
+								/ map.get(customer).get(product).get("Q2")
+										.getCount();
+						double maximum = 0.0;
+						for (double quant : map2.get(customer).get(product)
+								.get("Q2")) {
+							if (quant > maximum) {
+								maximum = quant;
+							}
+						}
+						for (double quant : map2.get(customer).get(product)
+								.get("Q1")) {
+							if (quant >= avg && quant <= maximum) {
+								before++;
+							}
+						}
+						for (double quant : map2.get(customer).get(product)
+								.get("Q3")) {
+							if (quant >= avg && quant <= maximum) {
+								after++;
+							}
+						}
+						System.out.printf("%-8s %-7s %-7s %10s %9s %n",
+								customer, product, quarter, before, after);
+					} else if (quarter.equals("Q3")) {
+						int before = 0;
+						int after = 0;
+						double avg = map.get(customer).get(product).get("Q3")
+								.getQuantity()
+								/ map.get(customer).get(product).get("Q3")
+										.getCount();
+						double maximum = 0.0;
+						for (double d : map2.get(customer).get(product)
+								.get("Q3")) {
+							if (d > maximum) {
+								maximum = d;
+							}
+						}
+						for (double quant : map2.get(customer).get(product)
+								.get("Q2")) {
+							if (quant >= avg && quant <= maximum) {
+								before++;
+							}
+						}
+						for (double quant : map2.get(customer).get(product)
+								.get("Q4")) {
+							if (quant >= avg && quant <= maximum) {
+								after++;
+							}
+						}
+						System.out.printf("%-8s %-7s %-7s %10s %9s %n",
+								customer, product, quarter, before, after);
+
+					} else if (quarter.equals("Q4")) {
+						int before = 0;
+						double avg = map.get(customer).get(product).get("Q4")
+								.getQuantity()
+								/ map.get(customer).get(product).get("Q4")
+										.getCount();
+						double maximum = 0.0;
+						for (double quant : map2.get(customer).get(product)
+								.get("Q4")) {
+							if (quant > maximum) {
+								maximum = quant;
+							}
+						}
+						for (double d : map2.get(customer).get(product)
+								.get("Q3")) {
+							if (d >= avg && d <= maximum) {
+								before++;
+							}
+						}
+						System.out.printf("%-8s %-7s %-7s %10s %9s %n",
+								customer, product, quarter, before,
+								"    <NULL>");
+					}
+				}
+			}
+		}
+
+	}
+}
